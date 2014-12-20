06d9055ef394a53fc1ce75b14039c989c748f536
Camilo Delvasto <camilodelvasto@gmail.com>
2014-12-20 18:41:27 +0100

    footer fixes

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@ var $ = jQuery.noConflict();
 $(document).ready(function(){
 
 
-	var aChildren = $("#sub_menu1 li").children(); // find the a children of the list items
+	var aChildren = $(".av-submenu-container").find('li').children(); // find the a children of the list items
 	var aArray = []; // create the empty aArray
 	for (var i=0; i < aChildren.length; i++) {    
 		var aChild = aChildren[i];
@@ -35,37 +35,46 @@ $(document).ready(function(){
 		}
 
 		if(windowPos + windowHeight == docHeight) {
-			if (!$("#sub_menu1 li:last-child a").hasClass("nav-active")) {
+			if (!$(".av-submenu-container li:last-child a").hasClass("nav-active")) {
 				var navActiveCurrent = $(".nav-active").attr("href");
 				$("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-				$("#sub_menu1 li:last-child a").addClass("nav-active");
+				$(".av-submenu-container li:last-child a").addClass("nav-active");
 			}
 		}
 	});
 
+	if($('.av-sticky-submenu')){
+		var rr = 85;
+		var gg = 0;
+		var bb = 83;
+		$('.av-sticky-submenu').css({'background': rgbToHex(rr,gg,bb)});
+		$('.av-sticky-submenu').find('li').each(function(){
+			$(this).css({'background': rgbToHex(rr,gg,bb)});
+			$(this).find('a').css({'background': rgbToHex(rr,gg,bb)});
 
-	var rr = 85;
-	var gg = 0;
-	var bb = 83;
-	$('#sub_menu1').css({'background': rgbToHex(rr,gg,bb)});
-	$('#sub_menu1').find('li').each(function(){
-		$(this).css({'background': rgbToHex(rr,gg,bb)});
-		$(this).find('a').css({'background': rgbToHex(rr,gg,bb)});
+			rr += 20;
+			gg += 3;
+			bb += 9;
+		})
+		
+		function componentToHex(c) {
+		    var hex = c.toString(16);
+		    return hex.length == 1 ? "0" + hex : hex;
+		}
 
-		rr += 20;
-		gg += 3;
-		bb += 9;
-	})
-	
-	function componentToHex(c) {
-	    var hex = c.toString(16);
-	    return hex.length == 1 ? "0" + hex : hex;
+		function rgbToHex(r, g, b) {
+		    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+		}
+
 	}
 
-	function rgbToHex(r, g, b) {
-	    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+
+	// attach a copy of the first menu to the fotter
+	if('.av-submenu-container'){
+		var newMenu = $('.av-submenu-container').first().clone();
+		$(newMenu).attr('id','footer-menu-bw');
+		$(newMenu).removeClass('av-sticky-submenu');
+		$(newMenu).prependTo('#footer');
 	}
-
-
 });
 
