6d5836ce1dd0e67cc8e738959ade0906b5c4e0ac
Camilo Delvasto <camilodelvasto@gmail.com>
2014-12-22 13:34:21 +0100

    better function structure on custom.js

--- js/custom.js
+++ js/custom.js
@@ -43,62 +43,65 @@ $(document).ready(function(){
 		}
 	});
 
-	var base_colored_menu = function(menuID,hexStart,hexEnd){
-		if($(menuID)){
-			var rgb = [hexToRgb(hexStart).r,hexToRgb(hexStart).g,hexToRgb(hexStart).b];
-			var totalItems = $(menuID).find('li').length-1;
-			var diff = [(hexToRgb(hexEnd).r - hexToRgb(hexStart).r)/totalItems, (hexToRgb(hexEnd).g - hexToRgb(hexStart).g)/totalItems,(hexToRgb(hexEnd).b - hexToRgb(hexStart).b)/totalItems];
-			$(menuID).css({'background': hexStart});
-			$(menuID).parents('.av-submenu-container').css({'background': hexStart});
-			$('<span class="menu_supplement" style="background: '+hexEnd+'"></span>').insertAfter($(menuID).find('.av-subnav-menu'));
-			$(menuID).find('li').each(function(){
-				$(this).css({'background': rgbToHex(rgb[0],rgb[1],rgb[2])});
-				$(this).find('a').css({'background': rgbToHex(rgb[0],rgb[1],rgb[2])});
-				rgb[0] += Math.round(diff[0]);
-				rgb[1] += Math.round(diff[1]);
-				rgb[2] += Math.round(diff[2]);
-			})
-			
-			function componentToHex(c) {
-			    var hex = c.toString(16);
-			    return hex.length == 1 ? "0" + hex : hex;
-			}
-
-			function rgbToHex(r, g, b) {
-			    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-			}
-			function hexToRgb(hex) {
-			    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-			    return result ? {
-			        r: parseInt(result[1], 16),
-			        g: parseInt(result[2], 16),
-			        b: parseInt(result[3], 16)
-			    	} : null;
-			}
-
-		}
-	}
 
 	// attach a copy of the first menu to the fotter
-	if('.av-submenu-container'){
-		var newMenu = $('.av-submenu-container').first().clone();
-		$(newMenu).attr('id','footer-menu-bw');
-		$(newMenu).removeClass('av-sticky-submenu');
-		$(newMenu).prependTo('#footer');
-	}
-
-	//base_colored_menu('#sub_menu2','#550053','#a5889b');
-	$('.av-submenu-container').not('#footer-menu-bw').find(' > div').each(function(){
-		var color_start = $(this).data('color_start');
-		var color_end = $(this).data('color_end');
-		var color_selection = $(this).data('color_selection');
-		console.log(color_selection);
-		if (color_selection == 'custom-color-menu' && !(color_end || color_start)){
-			color_start =  '#550053';
-			color_end =  '#dd22aa';
+	var base_clone_main_menu_footer = function(){
+		if('.av-submenu-container'){
+			var newMenu = $('.av-submenu-container').first().clone();
+			$(newMenu).attr('id','footer-menu-bw');
+			$(newMenu).removeClass('av-sticky-submenu');
+			$(newMenu).prependTo('#footer');
 		}
-		base_colored_menu($(this),color_start,color_end);
-	})
+	}();
 
+	var base_color_menu_cards = function(){
+		var base_colored_menu = function(menuID,hexStart,hexEnd){
+			if($(menuID)){
+				var rgb = [hexToRgb(hexStart).r,hexToRgb(hexStart).g,hexToRgb(hexStart).b];
+				var totalItems = $(menuID).find('li').length-1;
+				var diff = [(hexToRgb(hexEnd).r - hexToRgb(hexStart).r)/totalItems, (hexToRgb(hexEnd).g - hexToRgb(hexStart).g)/totalItems,(hexToRgb(hexEnd).b - hexToRgb(hexStart).b)/totalItems];
+				$(menuID).css({'background': hexStart});
+				$(menuID).parents('.av-submenu-container').css({'background': hexStart});
+				$('<span class="menu_supplement" style="background: '+hexEnd+'"></span>').insertAfter($(menuID).find('.av-subnav-menu'));
+				$(menuID).find('li').each(function(){
+					$(this).css({'background': rgbToHex(rgb[0],rgb[1],rgb[2])});
+					$(this).find('a').css({'background': rgbToHex(rgb[0],rgb[1],rgb[2])});
+					rgb[0] += Math.round(diff[0]);
+					rgb[1] += Math.round(diff[1]);
+					rgb[2] += Math.round(diff[2]);
+				})
+				
+				function componentToHex(c) {
+				    var hex = c.toString(16);
+				    return hex.length == 1 ? "0" + hex : hex;
+				}
+
+				function rgbToHex(r, g, b) {
+				    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+				}
+				function hexToRgb(hex) {
+				    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+				    return result ? {
+				        r: parseInt(result[1], 16),
+				        g: parseInt(result[2], 16),
+				        b: parseInt(result[3], 16)
+				    	} : null;
+				}
+
+			}
+		}
+
+		$('.av-submenu-container').not('#footer-menu-bw').find(' > div').each(function(){
+			var color_start = $(this).data('color_start');
+			var color_end = $(this).data('color_end');
+			var color_selection = $(this).data('color_selection');
+			console.log(color_selection);
+			if (color_selection == 'custom-color-menu' && !(color_end || color_start)){
+				color_start =  '#550053';
+				color_end =  '#dd22aa';
+			}
+			base_colored_menu($(this),color_start,color_end);
+		})
+	}();
 });
 
